fa183ea48bf790b0b804c554d83181763c0a8c5f
Agzdjy <agzdjyxyy@outlook.com>
2015-02-04 19:01:02 +0800

    验证码改为数字加字母且难度等级可调，更换图片背景

--- src/com/xeon/action/VerificServlet.java
+++ src/com/xeon/action/VerificServlet.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpSession;
  * 
  * @author Agzdjy
  * @生成验证码的servlet 采用servlet是为了与linux兼容
+ * @param level是难度等级，可自由选择，可自由选择验证码中字符数量 通过length参数指定
  *
  */
 public class VerificServlet extends HttpServlet {
@@ -25,25 +27,26 @@ public class VerificServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 8911596981927849417L;
-	private int width = 60;
-	private int heigh = 20;
+	private int width = 70;
+	private int heigh = 24;
 	
 	public void service(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		BufferedImage buffImg = new BufferedImage(width,heigh,BufferedImage.TYPE_INT_RGB);
 		Graphics2D g = buffImg.createGraphics();
 		Random random = new Random();
-		
+		//绘制图形底板色彩
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, width, heigh);
 		
-		Font font = new Font("Times new  Roman",Font.PLAIN,18);
+		Font font = new Font("Arial", Font.BOLD, heigh - 2);
 		g.setFont(font);
-		g.setColor(Color.BLACK);
-		g.drawRect(0, 0, width-1, heigh-1);
+		g.setColor(Color.LIGHT_GRAY);
+		g.drawRect(0, 0, width - 1, heigh -1);
 		
-		g.setColor(Color.GRAY);
+		g.setColor(Color.LIGHT_GRAY);
 		
+		//干扰线
 		for(int i =0;i<160;i++){
 			int x = random.nextInt(width);
 			int y = random.nextInt(heigh);
@@ -52,21 +55,19 @@ public class VerificServlet extends HttpServlet {
 			g.drawLine(x,y,x+x1,y+y1);
 		}
 		
-		StringBuffer randomCode = new StringBuffer();
-		int red = 0,green = 0,blue = 0;
-		for(int i=0;i<4;i++){
-			String strRand = String.valueOf(random.nextInt(10));
-			red = random.nextInt(110);
-			green = random.nextInt(50);
-			blue =random.nextInt(50);
-			
-			g.setColor(new Color(red,green,blue));
-			g.drawString(strRand, 13*i+6, 16);
-			randomCode.append(strRand);
+		//将验证码绘制到图片上
+		//这里面的4是指验证码有四个字符，可随意调整
+		String randomCode = getSecurityCode(4,SecurityCodeLevel.Medium,false);
+		g.setColor(new Color(19,34,246));
+		g.setFont(new Font("Georgia", Font.BOLD, 15));
+		int codeY = heigh - 10;
+		for(int i=0;i<randomCode.length();i++){
+			g.drawString(String.valueOf(randomCode.charAt(i)), i * 16 + 5, random.nextInt(2)+codeY);
 		}
-		
+		//释放资源
+		g.dispose();
 		HttpSession session = req.getSession();
-		session.setAttribute("VERIFYCODE", randomCode.toString());
+		session.setAttribute("VERIFYCODE", randomCode);
 		
 		resp.setHeader("Pragma", "no-cache");
 		resp.setHeader("Cache-Control", "no-cache");
@@ -76,5 +77,52 @@ public class VerificServlet extends HttpServlet {
 		ImageIO.write(buffImg, "jpeg", sos);
 		sos.close();
 	}
+	/**
+	 * 
+	 * @author Agzdjy
+	 * 生成数字
+	 *
+	 */
+	//难度等级
+	public enum SecurityCodeLevel {Simple,Medium,Hard};
+	public static String getSecurityCode(int length,SecurityCodeLevel level,boolean isCanRepeat){
+        int len=length;//4
+        char[] codes={'1','2','3','4','5','6','7','8','9',
+                      'a','b','c','d','e','f','g','h','i',
+                      'j','k','m','n','p','q','r','s','t',
+                      'u','v','w','x','y','z','A','B','C',
+                      'D','E','F','G','H','I','J','K','L',
+                      'M','N','P','Q','R','S','T','U','V',
+                      'W','X','Y','Z'};
+        if(level==SecurityCodeLevel.Simple){
+            codes=Arrays.copyOfRange(codes, 0,9);
+        }else if(level==SecurityCodeLevel.Medium){
+            codes=Arrays.copyOfRange(codes, 0,33);
+        }
+        int n=codes.length;
+        if(len>n&&isCanRepeat==false){
+            throw new RuntimeException(
+                    String.format("调用SecurityCode.getSecurityCode(%1$s,%2$s,%3$s)出现异常，" +
+                                   "当isCanRepeat为%3$s时，传入参数%1$s不能大于%4$s",
+                                   len,level,isCanRepeat,n));
+        }
+        char[] result=new char[len];
+        if(isCanRepeat){
+            for(int i=0;i<result.length;i++){
+                int r=(int)(Math.random()*n);
+                result[i]=codes[r];
+            }
+        }else{
+            for(int i=0;i<result.length;i++){
+                int r=(int)(Math.random()*n);
+                result[i]=codes[r];
+                codes[r]=codes[n-1];
+                n--;
+            }
+        }
+        
+        return String.valueOf(result);
+    }
+	
 	
 }
